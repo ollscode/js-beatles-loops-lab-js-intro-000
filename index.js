738cf7cee374216ecebb43cb1f035ea4ce591be6
Oscar <mundonatural@gmail.com>
2020-02-11 08:30:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,37 +13,39 @@ function theBeatlesPlay(musicians, instruments) {
   return frases;
 }
 
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-function johnLennonFacts(facts) {
-  var devuelve = [];
-
-  // esta es otra posible solucion
-  // for (var i = 0; i < facts.length; i++) {
-  //   devuelve.push(`${facts[i]}!!!`);
-  // }
-
-  while (i < facts.length) {
-    devuelve.push(`${facts[i]}!!!`);
-    i++;
-  }
-
-  return devuelve;
-}
-
-function iLoveTheBeatles(nro) {
-  var devuelve = [];
-  var i = 15 - nro;
-
-  do {
-    devuelve.push("I love the Beatles!");
-    i++;
-  } while (i < 15);
-
-  return devuelve;
-}
+// var facts = [
+//   "He was the last Beatle to learn to drive",
+//   "He was never a vegetarian",
+//   "He was a choir boy and boy scout",
+//   "He hated the sound of his own voice"
+// ];
+// 
+// function johnLennonFacts(facts) {
+//   var devuelve = [];
+// 
+//   // esta es otra posible solucion
+//   // for (var i = 0; i < facts.length; i++) {
+//   //   devuelve.push(`${facts[i]}!!!`);
+//   // }
+// 
+//   var i = 0;
+// 
+//   while (i < facts.length) {
+//     devuelve.push(`${facts[i]}!!!`);
+//     i++;
+//   }
+// 
+//   return devuelve;
+// }
+// 
+// function iLoveTheBeatles(nro) {
+//   var devuelve = [];
+//   var i = 15 - nro;
+// 
+//   do {
+//     devuelve.push("I love the Beatles!");
+//     i++;
+//   } while (i < 15);
+// 
+//   return devuelve;
+// }
