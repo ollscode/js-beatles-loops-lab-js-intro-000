7b07f5aefa8db0f55fa6a341ec08b43a9021097f
Oscar <mundonatural@gmail.com>
2020-02-11 08:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ function theBeatlesPlay(musicians, instruments) {
 
   return frases;
 }
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var devuelve = [];
+
+  for (var i = 0; i < facts.length; i++) {
+    devuelve.push(`${facts[i]}!!!`);
+  }
+
+  return devuelve;
+}
